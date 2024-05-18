21946798a2d0db0a8a114feb8fc3fe101373975c
yuzuriaichi <168105949+yuzuriaichi@users.noreply.github.com>
2024-05-19 02:53:56 +0800

    commit new

---
+++ BukuTextLy_admins/bukutextly_admins/android/app/src/main/kotlin/com/example/bukutextly_admins/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bukutextly_admins
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ BukuTextLy_users/bukutextly_users/android/app/src/main/kotlin/com/example/bukutextly_users/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bukutextly_users
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
