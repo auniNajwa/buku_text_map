2498cd7c41cfacf125f1e40d762dc5800f323722
auniNajwa <auninajwa02alizar@gmail.com>
2024-05-24 20:38:45 +0800

    changes
    a few changes

--- BukuTextLy_users/bukutextly_users/lib/pages/profile_page.dart
+++ BukuTextLy_users/bukutextly_users/lib/pages/profile_page.dart
@@ -35,200 +35,209 @@ class _ProfilePageState extends State<ProfilePage> {
               .doc(currentUser.email)
               .snapshots(),
           builder: (context, snapshot) {
-            //get user data
+            // Check for data
             if (snapshot.hasData) {
-              final userData = snapshot.data!.data() as Map<String, dynamic>;
+              // Ensure the data is not null
+              final data = snapshot.data!.data();
+              if (data != null) {
+                final userData = data as Map<String, dynamic>;
 
-              return Stack(
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      //profile picture
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          //profile picture
-                          Padding(
-                            padding: const EdgeInsets.only(left: 20.0),
-                            child: Container(
-                              width: 100, // Adjust size as needed
-                              height: 100, // Adjust size as needed
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: Colors.brown[
-                                    300], // Choose your desired background color
-                              ),
-                              child: const Icon(
-                                Icons.person,
-                                size: 73,
-                              ),
-                            ),
-                          ),
-
-                          //user's name
-                          Text(
-                            userData['first_name'] +
-                                ' ' +
-                                userData['last_name'],
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Colors.grey[800],
-                              fontSize: 20.0,
-                            ),
-                          ),
-
-                          //settings
-                          Padding(
-                            padding: const EdgeInsets.only(right: 20.0),
-                            child: IconButton(
-                              onPressed: () {
-                                Navigator.pushNamed(context, '/settingspage');
-                              },
-                              icon: const Icon(
-                                Icons.settings,
-                                size: 35,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-
-                      //username
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          top: 20,
-                          left: 50.0,
+                return Stack(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(
+                          height: 20,
                         ),
-                        child: Text(
-                          '@' + userData['username'],
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: Colors.grey[700],
-                            fontSize: 15.0,
-                          ),
-                        ),
-                      ),
-
-                      //rating thingy
-                      Padding(
-                        padding: const EdgeInsets.only(left: 30.0, top: 10),
-                        child: Row(
+                        //profile picture
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            RatingBar.builder(
-                              initialRating: ratingValue,
-                              minRating: 0,
-                              direction: Axis.horizontal,
-                              allowHalfRating: true,
-                              itemPadding:
-                                  const EdgeInsets.symmetric(horizontal: 5),
-                              itemSize: 25,
-                              itemBuilder: (context, _) => const Icon(
-                                Icons.star_rate_rounded,
-                                color: Colors.amber,
+                            //profile picture
+                            Padding(
+                              padding: const EdgeInsets.only(left: 20.0),
+                              child: Container(
+                                width: 100, // Adjust size as needed
+                                height: 100, // Adjust size as needed
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.brown[
+                                      300], // Choose your desired background color
+                                ),
+                                child: const Icon(
+                                  Icons.person,
+                                  size: 73,
+                                ),
                               ),
-                              onRatingUpdate: (rating) {
-                                print(rating);
-                                setState(() {
-                                  ratingValue = rating;
-                                });
-                              },
                             ),
-                            const SizedBox(width: 10),
+
+                            //user's name
                             Text(
-                              ratingValue
-                                  .toString(), // Display the rating value as a string
-                              style: const TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
+                              userData['first_name'] +
+                                  ' ' +
+                                  userData['last_name'],
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Colors.grey[800],
+                                fontSize: 20.0,
+                              ),
+                            ),
+
+                            //settings
+                            Padding(
+                              padding: const EdgeInsets.only(right: 20.0),
+                              child: IconButton(
+                                onPressed: () {
+                                  Navigator.pushNamed(context, '/settingspage');
+                                },
+                                icon: const Icon(
+                                  Icons.settings,
+                                  size: 35,
+                                ),
                               ),
                             ),
                           ],
                         ),
-                      ),
 
-                      //Featurs box
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          top: 15.0,
-                          left: 10.0,
-                          right: 10.0,
-                          bottom: 10.0,
-                        ),
-                        child: SizedBox(
-                          height: 100,
-                          child: GridView.builder(
-                            shrinkWrap: true,
-                            itemCount: 4,
-                            gridDelegate:
-                                const SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 4,
-                              crossAxisSpacing: 10,
-                              mainAxisSpacing: 10,
+                        //username
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            top: 20,
+                            left: 50.0,
+                          ),
+                          child: Text(
+                            '@' + userData['username'],
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.grey[700],
+                              fontSize: 15.0,
                             ),
-                            itemBuilder: (context, index) {
-                              return Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: FeaturesBoxWidget(
-                                  icon: IconButton(
-                                    onPressed: () {},
-                                    icon: const Icon(Icons.star_border_rounded),
-                                  ),
+                          ),
+                        ),
+
+                        //rating thingy
+                        Padding(
+                          padding: const EdgeInsets.only(left: 30.0, top: 10),
+                          child: Row(
+                            children: [
+                              RatingBar.builder(
+                                initialRating: ratingValue,
+                                minRating: 0,
+                                direction: Axis.horizontal,
+                                allowHalfRating: true,
+                                itemPadding:
+                                    const EdgeInsets.symmetric(horizontal: 5),
+                                itemSize: 25,
+                                itemBuilder: (context, _) => const Icon(
+                                  Icons.star_rate_rounded,
+                                  color: Colors.amber,
                                 ),
-                              );
-                            },
+                                onRatingUpdate: (rating) {
+                                  print(rating);
+                                  setState(() {
+                                    ratingValue = rating;
+                                  });
+                                },
+                              ),
+                              const SizedBox(width: 10),
+                              Text(
+                                ratingValue
+                                    .toString(), // Display the rating value as a string
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                      ),
 
-                      //search bar
-                      const Padding(
-                        padding: EdgeInsets.all(10.0),
-                        child: SearchBar(
-                          hintText: 'Search',
-                        ),
-                      ),
-
-                      //item listing
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          top: 15.0,
-                          left: 20.0,
-                          right: 20.0,
-                          bottom: 15.0,
-                        ),
-                        child: SizedBox(
-                          height: 700, // Adjust the height as needed
-                          child: GridView.builder(
-                            shrinkWrap: true,
-                            itemCount: 10,
-                            gridDelegate:
-                                const SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 2,
-                              crossAxisSpacing: 15,
-                              mainAxisSpacing: 15,
+                        //Featurs box
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            top: 15.0,
+                            left: 10.0,
+                            right: 10.0,
+                            bottom: 10.0,
+                          ),
+                          child: SizedBox(
+                            height: 100,
+                            child: GridView.builder(
+                              shrinkWrap: true,
+                              itemCount: 4,
+                              gridDelegate:
+                                  const SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 4,
+                                crossAxisSpacing: 10,
+                                mainAxisSpacing: 10,
+                              ),
+                              itemBuilder: (context, index) {
+                                return Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: FeaturesBoxWidget(
+                                    icon: IconButton(
+                                      onPressed: () {},
+                                      icon: const Icon(Icons.star_border_rounded),
+                                    ),
+                                  ),
+                                );
+                              },
                             ),
-                            itemBuilder: (context, index) {
-                              return const ItemBoxWidget(
-                                textInSquare: 'item description lorem ipsum',
-                                iconData: Icons.favorite,
-                              );
-                            },
                           ),
                         ),
-                      ),
 
-                      const SizedBox(height: 20),
-                    ],
-                  ),
-                ],
-              );
+                        //search bar
+                        const Padding(
+                          padding: EdgeInsets.all(10.0),
+                          child: SearchBar(
+                            hintText: 'Search',
+                          ),
+                        ),
+
+                        //item listing
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            top: 15.0,
+                            left: 20.0,
+                            right: 20.0,
+                            bottom: 15.0,
+                          ),
+                          child: SizedBox(
+                            height: 700, // Adjust the height as needed
+                            child: GridView.builder(
+                              shrinkWrap: true,
+                              itemCount: 10,
+                              gridDelegate:
+                                  const SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 2,
+                                crossAxisSpacing: 15,
+                                mainAxisSpacing: 15,
+                              ),
+                              itemBuilder: (context, index) {
+                                return const ItemBoxWidget(
+                                  textInSquare: 'item description lorem ipsum',
+                                  iconData: Icons.favorite,
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+
+                        const SizedBox(height: 20),
+                      ],
+                    ),
+                  ],
+                );
+              } else {
+                // Handle the case where the data is null
+                return Center(
+                  child: Text('User data not found'),
+                );
+              }
             } else if (snapshot.hasError) {
               return Center(
-                child: Text('Error${snapshot.error}'),
+                child: Text('Error: ${snapshot.error}'),
               );
             }
 
